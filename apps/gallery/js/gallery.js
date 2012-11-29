0b87ac60f8939df0051c4bc496c57e2661e61f1e
Dominic Kuo <dkuo@mozilla.com>
2012-11-29 16:44:37 +0800

    Bug 814323 - add filepaths for bluetooth workaround

--- apps/gallery/js/gallery.js
+++ apps/gallery/js/gallery.js
@@ -882,14 +882,21 @@ function share(blobs) {
   if (blobs.length === 0)
     return;
 
-  var names = [], types = [];
+  var names = [], types = [], fullpaths = [];
 
   // Get the file name (minus path) and type of each blob
   blobs.forEach(function(blob) {
     // Discard the path, we just want the base name
     var name = blob.name;
-    if (name)
+    if (name) {
+      // We try to fix Bug 814323 by using
+      // current workaround of bluetooth transfer
+      // so we will pass both filenames and fullpaths
+      // The fullpaths can be removed after Bug 811615 is fixed
+      fullpaths.push(name);
       name = name.substring(name.lastIndexOf('/') + 1);
+    }
+
     names.push(name);
 
     // And we just want the first component of the type "image" or "video"
@@ -915,7 +922,8 @@ function share(blobs) {
       type: type,
       number: blobs.length,
       blobs: blobs,
-      filenames: names
+      filenames: names,
+      filepaths: fullpaths
     }
   });
 
